c13bc3212a8a3407569dfb54b6eb55f495549b08
Andressa Santos <andressasantosp0@gmail.com>
2021-04-20 15:02:52 -0300

    javascript

--- fundamentos-web/assets/js/script.js
+++ fundamentos-web/assets/js/script.js
@@ -1,6 +1,10 @@
 let nome = window.document.getElementById("nome");
 let email = document.querySelector("#email");
 let assunto = document.querySelector("#assunto");
+let nomeOk = false;
+let emailOk = false;
+let assuntoOk = false;
+let mapa = document.querySelector("#mapa");
 
 function validaNome() {
     let txtNome = document.querySelector("#txtNome");
@@ -8,15 +12,46 @@ function validaNome() {
         txtNome.innerHTML = "Nome inválido";
     } else {
         txtNome.remove();
+        nomeOk = true;
     }
 }
 
 function validaEmail() {
     let txtEmail = document.querySelector("#txtEmail");
 
-    if (email.value.indexOf("@") == -1 || email.value.indexOf('.') == -1) {
+    if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
         txtEmail.innerHTML = "Email inválido";
     } else {
         txtEmail.remove();
+        emailOk = true;
     }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector("#txtAssunto");
+    if (assunto.value.length >= 240) {
+        txtAssunto.innerHTML = "Digite no máximo 240 caracteres";
+        txtAssunto.style.display = "block";
+    } else {
+        txtAssunto.style.display = "none";
+        assuntoOk = ok;
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert("Formulário enviado com sucesso!");
+    } else {
+        alert("Preencha o formulário corretamente");
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaZoomOut() {
+    mapa.style.width = '600px'
+    mapa.style.height = '450px'
 }
